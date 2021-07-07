945a0092ea0e8d3594db3594f53ea4c528f9f517
Brown (QUINTON), Lassiter <LassiterBrown@quickenloans.com>
2021-07-07 13:46:01 -0400

    remaking the repo

---
+++ kalendarium/Controllers/EventController.cs
@@ -0,0 +1,54 @@
+﻿using kalendarium.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace kalendarium.Controllers
+{
+    [Route("event")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+      
+
+
+        [HttpPost("add/{}/{}/{}/{}")]
+        public bool AddEvent(int user_id, string name, bool privateEvent, DateTime dt_id)
+        {
+            DAL.AddEvent(user_id, name, privateEvent, dt_id);
+            return true;
+        }
+
+    }
+}
+
+
+
+/* [HttpGet]
+       public async Task<string> GetAll()
+       {
+           HttpClient client = new HttpClient();
+           client.BaseAddress = new Uri("https://grandcircusco.github.io");
+           var response = await client.GetAsync("/demo-apis/donuts.json");
+           string json = await response.Content.ReadAsStringAsync();
+           return json;
+       }
+
+       // Get Specific Donut
+       // Our route: GET localhost:12345/donut/3
+       //https://grandcircusco.github.io/demo-apis/donuts/2.json
+       [HttpGet("{id}")]
+       public async Task<string> GetOne(int id)
+       {
+           HttpClient client = new HttpClient();
+           client.BaseAddress = new Uri("https://grandcircusco.github.io");
+           var response = await client.GetAsync($"/demo-apis/donuts/{id}.json");
+           string json = await response.Content.ReadAsStringAsync();
+           return json;
+       }
+
+       */
---
+++ kalendarium/Models/DAL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace kalendarium.Models
+{
+
+    public class DAL
+    {
+        static public IDbConnection db;
+
+        // Department methods
+        // C - Add Event 
+        // R: Read All, Read one by ID 
+        // U -- Update Location / Event ID? 
+        // D -- Delete
+
+
+        public static Event AddEvent(int user_id, string name, bool privateEvent, DateTime dt_id)
+        {
+            Event newevent = new Event() { id = user_id, name = name, privateEvent = privateEvent, dt_id = dt_id };
+            db.Insert(newevent);
+            return newevent;
+        }
+
+        public static User AddUser( string firstName, string lastName, string emailAddress,)
+        {
+            Event newevent = new Event() { id = user_id, name = firstname, privateEvent = privateEvent, dt_id = dt_id };
+            db.Insert(newevent);
+            return newevent;
+        }
+
+
+        public static string UpdateEvent(Event updatedevent)
+        {
+            db.Update(updatedevent);
+            return "Update Worked!";
+        }
+
+        public static List<Event> ReadAllEvents()
+        {
+
+            return db.GetAll<Event>().ToList();
+        }
+
+    
+        public static Event ReadOneEvent(int id)
+        {
+            return db.Get<Event>(id);
+        }
+
+        public static void DeleteDepartment(int id)
+        {
+            Event tempobj = new Event();
+            tempobj.id = id;
+            db.Delete(tempobj);
+        }
+
+
+    }
+}
+
